c77d35443c4d2f9d712ddc9851b33d59a699ceb9
kouki223 <kouki.1818194@gmail.com>
2025-06-19 16:07:41 +0900

    docs:型に応じて実行結果が変更される

--- increment.js
+++ increment.js
@@ -1,5 +1,14 @@
 function increment(num) {
     return num + 1;
-  }
-  console.log(increment(999));
-  
+}
+console.log(increment(999));
+//↓
+//実行した結果 => 9991
+//function increment(num) {
+//return num + 1;
+//}
+//console.log(increment(999));
+//↓
+//このコードを実行した結果は1000になる
+//型によって実行した結果が異なるという例
+//このように型によって求めた値を得る事ができないという事を避けるためにTSがある
