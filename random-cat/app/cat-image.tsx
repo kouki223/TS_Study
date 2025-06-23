a0d37dd8ac0756d79f6a5ecf5f887f3ac95fe2ff
kouki223 <kouki.1818194@gmail.com>
2025-06-23 15:37:10 +0900

    CatAPIを叩くアプリケーションの作成＋追加で名前（ID）も表示されるようにしてスタイルを調整した

--- random-cat/app/cat-image.tsx
+++ random-cat/app/cat-image.tsx
@@ -3,25 +3,35 @@
 
 import { useState } from "react"
 import { fetchImage } from "./fetch-image"
+import styles from "./page.module.css"
 
 type CatImageProps = {
     url: string;
-}
+    id: string;
+};
 
-export function CatImage({url}: CatImageProps) {
+export function CatImage({ url, id }: CatImageProps) {
     const [imageUrl, setImageUrl] = useState(url);
+    const [imageId, setImageId] = useState(id);
 
     const freshImage = async() => {
         setImageUrl("");
         const image = await fetchImage();
         setImageUrl(image.url);
+        setImageId(image.id);
     };
 
     return (
-        <div>
-            {imageUrl && <img src={imageUrl} />}
-            <button onClick={freshImage}>他のニャンコも見る</button>
-            <h1></h1>
-        </div>
+        <>
+            <h1 className={styles.h1}>この猫のIDは{imageId}</h1>
+            <div className={styles.page}>
+                <button onClick={freshImage} className={styles.button}>
+                    他のニャンコも見る
+                </button>
+                <div className={styles.frame}>
+                    {imageUrl && <img src={imageUrl} className={styles.img}/>}
+                </div>
+            </div>
+        </>
     )
 };
--- random-cat/app/page.tsx
+++ random-cat/app/page.tsx
@@ -5,8 +5,7 @@ export default async function Home() {
   const image = await fetchImage();
   return (
     <>
-      <h1>The cat ID is {image.id}</h1>
-      <CatImage url={image.url} />
+      <CatImage url={image.url} id={image.id}/>
     </>
   )
 };
